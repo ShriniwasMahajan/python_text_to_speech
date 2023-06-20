61e641ff445451c23aca18b38eb23dac1823f603
Shriniwas Mahajan <mahajan.shriniwas7@gmail.com>
2023-06-20 18:07:57 +0530

    Update Text to Speech.py

--- Text to Speech.py
+++ Text to Speech.py
@@ -10,7 +10,7 @@ from gtts import gTTS
 import os
 
 # print all available languages
-print("List of all available languages:")
+print("List of all available languages: ")
 print(googletrans.LANGUAGES)
 
 # create recognizer() class objects
@@ -18,7 +18,7 @@ recog1 = spr.Recognizer()
 recog2 = spr.Recognizer()
 
 # create microphone instance with device microphone
-micro = spr.Microphone(device_index=0)
+micro = spr.Microphone(device_index = 0)
 # p = pyaudio.PyAudio()
 # info = p.get_host_api_info_by_index(0)
 # numdevices = info.get('deviceCount')
@@ -30,12 +30,12 @@ micro = spr.Microphone(device_index=0)
 # myPyAudio.get_device_count()
 
 # taking language as input
-from_lang = input("Enter the code of the language to be translated:")
-to_lang = input("Enter the code of the your preferred language:")
+from_lang = input("Enter the code of the language to be translated: ")
+to_lang = input("Enter the code of the preferred language to be translated into: ")
 
 # capture voice
 with micro as source:
-    print("Speak 'hello' to initiate the translation!")
+    print("Speak 'hello' out loud to initiate the translation!")
     print("-------------------->")
     audio = recog1.listen(source)
 
@@ -44,23 +44,20 @@ if 'hello' in recog1.recognize_google(audio):
     # recog1 = spr.Recognizer()
     translator = Translator()
     with micro as source:
-        print('Speak a sentence...')
+        print("Speak a sentence...")
         audio = recog2.listen(source)
-        get_sentence = recog2.recognize_google(audio)
         try:
             get_sentence = recog2.recognize_google(audio)
-            default_text = (translator.translate(
-                get_sentence, src='en', dest=from_lang)).text
-            print("Phrase to be translated: "+default_text)
-            text_to_translate = translator.translate(
-                get_sentence, src=from_lang, dest=to_lang)
+            default_text = (translator.translate(get_sentence, src = 'en', dest = from_lang)).text
+            print("Phrase to be translated: " + default_text)
+            text_to_translate = translator.translate(get_sentence, src = from_lang, dest = to_lang)
             # print(type(text_to_translate))
             text = text_to_translate.text
-            print("The translated sentence is:"+text)
-            speak = gTTS(text=text, lang=to_lang, slow=False)
+            print("The translated sentence is: " + text)
+            speak = gTTS(text = text, lang = to_lang, slow = False)
             speak.save("captured_voice.mp3")
             os.system("start captured_voice.mp3")
         except spr.UnknownValueError:
             print("Unable to understand input")
         except spr.RequestError as e:
-            print("Unable to provide required output".format(e))
+            print("Unable to provide required output: {}".format(e))
